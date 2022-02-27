ada190b2fa9a6698227c3ea7909cf9d9eaccbffb
talha21232 <ta4240835@gmail.com>
2022-02-27 15:21:44 +0500

    login and signup updation

--- app/src/main/java/com/example/myapplication/verticlal.java
+++ app/src/main/java/com/example/myapplication/verticlal.java
@@ -5,23 +5,60 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class verticlal extends AppCompatActivity {
 
-    public Button button;
+    EditText mTextUsername;
+    EditText mTextPassword;
+    Button mButtonLogin;
+    TextView mTextViewRegister;
+    DatabaseHelper db;
+    ViewGroup progressView;
+    protected boolean isProgressShowing = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verticlal);
-        button= (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+
+
+
+        db = new DatabaseHelper(this);
+        mTextUsername = (EditText)findViewById(R.id.edittext_username);
+        mTextPassword = (EditText)findViewById(R.id.edittext_password);
+        mButtonLogin = (Button)findViewById(R.id.button_login);
+        mTextViewRegister = (TextView)findViewById(R.id.textview_register);
+        mTextViewRegister.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(verticlal.this, mainpage.class);
-                startActivity(intent);
+            public void onClick(View view) {
+                Intent registerIntent = new Intent(verticlal.this,regestrayion.class);
+                startActivity(registerIntent);
+            }
+        });
+
+        mButtonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user = mTextUsername.getText().toString().trim();
+                String pwd = mTextPassword.getText().toString().trim();
+                Boolean res = db.checkUser(user, pwd);
+                if(res == true)
+                {
+                    Intent HomePage = new Intent(verticlal.this,mainpage.class);
+                    startActivity(HomePage);
+                }
+                else
+                {
+                    Toast.makeText(verticlal.this,"password incorrect",Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
+
+
 }
