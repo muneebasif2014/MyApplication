1ebf097b518f3de7f86a3e2a4ecf1674dd0c2f70
muneebasif2014 <72495709+muneebasif2014@users.noreply.github.com>
2022-04-16 01:51:34 +0500

    firebase attaached data insertion done in laywer

--- app/src/main/java/com/example/myapplication/lawyer.java
+++ app/src/main/java/com/example/myapplication/lawyer.java
@@ -3,12 +3,47 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class lawyer extends AppCompatActivity {
+    Button b1;
+    EditText t1,t2,t3,t4,t5,t6,t7;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lawyer);
+
+        t1= (EditText) findViewById(R.id.lawerid);
+        t2= (EditText) findViewById(R.id.laywername);
+        t3= (EditText) findViewById(R.id.lawyernumber);
+        t4= (EditText) findViewById(R.id.layweradd);
+        t5= (EditText) findViewById(R.id.lawyercatag);
+        t6= (EditText) findViewById(R.id.laywerremarks);
+
+        b1= (Button) findViewById(R.id.button1);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String id = t1.getText().toString();
+                String name = t2.getText().toString();
+                String num = t3.getText().toString();
+                String addr = t4.getText().toString();
+                String catag = t5.getText().toString();
+                String rem = t6.getText().toString();
+
+                Lawyer_details_dataholder obj = new Lawyer_details_dataholder(name, num, addr,catag,rem);
+                FirebaseDatabase db = FirebaseDatabase.getInstance();
+                DatabaseReference node = db.getReference("Lawyer");
+                node.child(id).setValue(obj);
+                Toast.makeText(getApplicationContext(),"data insert", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
