2db9559c0463f596994d77af713ab61ec78ee048
muneebasif2014 <72495709+muneebasif2014@users.noreply.github.com>
2022-04-16 17:22:41 +0500

    firebase attaached data insertion done in schdual

--- app/src/main/java/com/example/myapplication/UPdate.java
+++ app/src/main/java/com/example/myapplication/UPdate.java
@@ -3,12 +3,31 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class UPdate extends AppCompatActivity {
+    EditText t1,t2,t3,t4,t5;
+    Button b2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update);
+
+        t1= (EditText) findViewById(R.id.case_status_up);
+        t2= (EditText) findViewById(R.id.up_CASe_num);
+        t3= (EditText) findViewById(R.id.up_case_name);
+        t4= (EditText) findViewById(R.id.up_cASe_date);
+        t5= (EditText) findViewById(R.id.Up_update);
+        b2= (Button) findViewById(R.id.button_up);
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/myapplication/clinte.java
+++ app/src/main/java/com/example/myapplication/clinte.java
@@ -3,12 +3,35 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class clinte extends AppCompatActivity {
+    EditText t1,t2,t3,t4,t5,t6;
+    Button b2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clinte);
+
+        t1= (EditText) findViewById(R.id.clinte_name);
+        t2= (EditText) findViewById(R.id.Add_clinte);
+        t3= (EditText) findViewById(R.id.city_clinte);
+        t4= (EditText) findViewById(R.id.num_clinte);
+        t5= (EditText) findViewById(R.id.emil_clinte);
+        t6= (EditText) findViewById(R.id.remarks_clinte);
+        b2= (Button) findViewById(R.id.button_clinte);
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
+
+
 }
