f518439593a6782caed8000af0ebdba8674c2dd4
muneebasif2014 <72495709+muneebasif2014@users.noreply.github.com>
2022-04-16 17:05:27 +0500

    firebase attaached data insertion done in schdual

--- app/src/main/java/com/example/myapplication/Schedule.java
+++ app/src/main/java/com/example/myapplication/Schedule.java
@@ -3,10 +3,16 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 public class Schedule extends AppCompatActivity {
+    EditText t1,t2,t3,t4,t5,t6;
+    Button b2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,19 @@ public class Schedule extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
 
         dropdown.setAdapter(adapter);
+        t1= (EditText) findViewById(R.id.case_status);
+        t2= (EditText) findViewById(R.id.clientname);
+        t3= (EditText) findViewById(R.id.casenum);
+        t4= (EditText) findViewById(R.id.casename);
+        t5= (EditText) findViewById(R.id.casedate);
+        t6= (EditText) findViewById(R.id.scremsrk);
+      b2= (Button) findViewById(R.id.button1);
+      b2.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+
+          }
+      });
+
     }
 }
